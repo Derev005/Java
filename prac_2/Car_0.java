39f24e8204dbf477256f490c227ded929c10738c
Kirill <118915590+Derev005@users.noreply.github.com>
2024-09-10 21:48:45 +0300

    Update Car_0.java

--- prac_2/Car_0.java
+++ prac_2/Car_0.java
@@ -1,1 +1,9 @@
-
+public class Car {
+    String model;
+    String license;
+    String color;
+    int year;
+    public Car(String model, String license, String color, int year) {};
+    public Car() {};
+    public Car(String model, String color) {};
+}
