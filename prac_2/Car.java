5c7cc9aa63dafe62473b19ee534e4ddf4f62ee23
Kirill <118915590+Derev005@users.noreply.github.com>
2024-09-10 22:16:01 +0300

    Update and rename Car.java to Car_0.java

--- prac_2/Car.java
+++
@@ -1,9 +0,0 @@
-public class Car {
-    String model;
-    String license;
-    String color;
-    int year;
-    public Car(String model, String license, String color, int year) {};
-    public Car() {};
-    public Car(String model, String color) {};
-}
---
+++ prac_2/Car_0.java
@@ -0,0 +1,9 @@
+public class Car_0 {
+    String model;
+    String license;
+    String color;
+    int year;
+    public Car_0(String model, String license, String color, int year) {};
+    public Car_0() {};
+    public Car_0(String model, String color) {};
+}
