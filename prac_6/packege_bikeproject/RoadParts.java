e351ee4e2a7ff0b04b19cd5f4fd3a8c6ebe3e332
Kirill <118915590+Derev005@users.noreply.github.com>
2024-10-15 18:34:18 +0300

    Create RoadParts.java

---
+++ prac_6/packege_bikeproject/RoadParts.java
@@ -0,0 +1,10 @@
+package bikeproject;
+
+public interface RoadParts {
+    String terrain = "track_racing";
+
+    String getTyreWidth();
+    void setTyreWidth(String TyreWidth);
+    String getPostHeight();
+    void setPostHeight(String PostHeight);
+}
