b83567323d80fbe49608740db35c3fc791b3cb72
Kirill <118915590+Derev005@users.noreply.github.com>
2024-10-20 08:58:31 +0300

    Create Circle.java

---
+++ prac_7/task_4/Circle.java
@@ -0,0 +1,46 @@
+public class Circle extends GeometricObject implements Colorable {
+    //---Fields of a class---
+    private double radius;
+    //---Methods of a class---
+    public double getRadius() { return radius; }
+    public void setRadius(double radius) { this.radius = radius; }
+
+    public double getArea() { return radius * radius * Math.PI; }
+
+    public double getDiameter() { return 2 * radius; }
+
+    public double getPerimeter() { return 2 * radius * Math.PI; }
+
+    public void printCircle() { System.out.println("Круг создан " + getDateCreated() + " и радиус равен " + radius); }
+
+    public void max(Circle circle) {
+        if (this.getArea() > circle.getArea()) { System.out.print("Первый объект больше второго!\n"); }
+        else { System.out.print("Второй объект больше или равен первому!\n"); }
+    }
+
+    public void compareTo(Rectangle rectangle) {
+        if (this.getArea() < rectangle.getArea()) {
+            System.out.print("Первый объект больше второго!\n");
+        }
+        else {
+            System.out.print("Второй объект больше или равен первому!\n");
+        }
+    }
+
+    public String toString() {
+        return "Круг:\n" + "Создан: " + getDateCreated() + "\nРадиус: " + radius + "\nЦвет: " + getColor() +
+                "\nЗаливка: " + isFilled() + "\nПериметр: " + getPerimeter() + "\nПлощадь: " + getArea() + "\n";
+    }
+    //---implementation to the interface---
+    public void howToColor() {
+        System.out.print("Расскрасте окружность!\n");
+    }
+    //---Constructors---
+    public Circle() { }
+    public Circle(double radius) { this.radius = radius; }
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        setColor(color);
+        setFilled(filled);
+    }
+}
