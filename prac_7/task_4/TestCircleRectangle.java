93b8893a564c18c0dc2b73d0330a24dd6a2de949
Kirill <118915590+Derev005@users.noreply.github.com>
2024-10-20 08:57:14 +0300

    Update TestCircleRectangle.java

--- prac_7/task_4/TestCircleRectangle.java
+++ prac_7/task_4/TestCircleRectangle.java
@@ -1,1 +1,19 @@
+public class TestCircleRectangle {
+    public static void main(String[] args) throws Triangle.IllegalArgumentException {
+        GeometricObject [] geometricObject = new GeometricObject[5];
 
+        geometricObject[0] = new Circle(5.0);
+        geometricObject[1] = new Rectangle(5.0, 4.0);
+        geometricObject[2] = new Triangle(3.0, 5.0, 4.0);
+        geometricObject[3] = new Square(6.0);
+        geometricObject[4] = new GeometricObject();
+
+        for (int i = 0; i < geometricObject.length; i++) {
+            System.out.println(geometricObject[i].toString());
+        }
+
+        for (int i = 0; i < geometricObject.length; i++) {
+            geometricObject[i].howToColor();
+        }
+    }
+}
